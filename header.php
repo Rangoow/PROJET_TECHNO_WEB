51e498f4299786212db41236f4219c91c158bc3d
Luffylosophe <lionelmbanza@gmail.com>
2019-10-04 16:32:30 +0200

    header to mock up
    test du header

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <title>header</title>
+    </head>
+
+    <body>
+
+    	<h1> PEZPAL </h1>
+    	<h3> ça pez ! </h3>
+    	
+
+
+        
+
+        
+    </body>
+</html>
