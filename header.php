9d7484334d95b3d0bc5af1ca50d189e06121b6f8
Rangoow <noe.esp@hotmail.fr>
2019-10-07 00:20:31 +0200

    header début + main + css

---
+++ header.php
@@ -0,0 +1,17 @@
+<header>
+    <a id="headerTitle" href="main.php">MyPez</a>
+    <nav>
+        <div class="menuCategory">
+            <a class="menuCategoryTitle" href="productPage.php">Our Products</a>
+        </div>
+        <div class="menuCategory">
+            <a class="menuCategoryTitle" href="searchPage.php">Search for Product</a>
+        </div>
+        <div class="menuCategory">
+            <a class="menuCategoryTitle" href="cartPage.php">Your Cart</a>
+        </div>
+        <div class="menuCategory">
+            <a class="menuCategoryTitle" href="createAccount.php">Create an Account</a>
+        </div>
+    </nav>
+</header>
---
+++ main.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MyPez</title>
+    <link rel="stylesheet" href="CSS\main.css">
+</head>
+<body>
+    <?php include 'header.php'?>
+</body>
+</html>
