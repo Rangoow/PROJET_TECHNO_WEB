b60f9e7758e491c1984bb7d3df0303fcf49cef44
Luffylosophe <lionelmbanza@gmail.com>
2019-10-06 21:55:50 +0200

    header myPEZ

--- header.php
+++ header.php
@@ -3,16 +3,40 @@
     <head>
         <!-- En-tête de la page -->
         <meta charset="utf-8" />
-        <title>header</title>
+        <link rel="stylesheet" href="headercss.css" />
+        <title>PEZPAL</title>
     </head>
 
     <body>
+        <img src="header_image.jpg" alt="photodepez" />
+    	<balise1> PEZPAL </balise1> 
+    	<balise2> ça pez ! </balise2>
 
-    	<h1> PEZPAL </h1>
-    	<h3> ça pez ! </h3>
+        <balise4> 
+         <a class="lien" href="productpage.php"> Nos Produits en vente</a> 
+         <a class="lien" href="searchpage.php"> | Rechercher un produit</a>
+        <balise4/>
+
+
+        <balise3>
+        <form method="post" action="productpage.php" >
+            <label for="nom">Pseudo ? : </label>
+            <input type="text" name="pseudo" id="pseudo"><br><br>
+
+            <label for="age">Mot de Passe ? :</label>
+            <input type="password" name="pseudo" id="pseudo"><br><br>
+        
+
+            <input type="submit" value="Connexion"/>
+          </form>
+
+        <balise3/>
+
+
+
+
+    
     	
-
-
         
 
         
