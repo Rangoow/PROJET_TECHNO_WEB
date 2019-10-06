f683f3969bd2cc99542c7eb1afb30dc04fff94be
Rangoow <noe.esp@hotmail.fr>
2019-10-06 22:37:34 +0200

    product page initialization

--- product.php
+++ product.php
@@ -1,15 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <!-- En-tête de la page -->
         <meta charset="utf-8" />
-        <title>header</title>
+        <title>Products</title>
     </head>
-<P> </P>
     <body>
-
-    	<h1> PEZPAL </h1>
-    	<h3> ça pez ! </h3>
+        <?php include 'header.php' ?>
     	
 
 
