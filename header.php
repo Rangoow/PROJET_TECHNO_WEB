a8993e4850766a4bb1faddd3e0341ff701e8a644
Rangoow <noe.esp@hotmail.fr>
2019-10-08 17:07:26 +0200

    header modif

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
 <ul class="navBar">
-  <li><a class="titleCategory" style="float:left" href="/">SIGN IN</a></li>
+<li><a class="titleCategory" style="float:left" href="/">SIGN IN</a></li>
   <li><a class="titleCategory" href="">PRODUCTS</a></li>
   <li><a class="titleCategory" href="">SEARCH</a></li>
   <li><a id="headerTitle" href="">MyPez</a></li>
