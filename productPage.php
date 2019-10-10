e1da1022e4f18ff21dd123e03845729632d00fc5
Rangoow <noe.esp@hotmail.fr>
2019-10-10 11:32:23 +0200

    products add & adjusts

--- productPage.php
+++ productPage.php
@@ -13,59 +13,38 @@
     <?php include 'header.php'?>
     <section class="productContainer"> 
     <div class="product-card">
-        <div ><img class="productimage" src="https://www.chilipeppermadness.com/wp-content/uploads/2019/04/Carolina-Reaper-Pepper.jpg"></div>   
+        <div ><img class="productimage" src="Image\CarolinaReaper.jpg"></div>   
         <div class="productinfo">
             <h5>Carolina Reaper</h5>
-            <h6>$99.99</h6>
+            <h6>14.99€</h6>
+            <button class="addtocartBtn">Add to Cart</button>
         </div>
     </div>
     <div class="product-card">
-        <div ><img class="productimage" src="https://www.chilipeppermadness.com/wp-content/uploads/2019/04/Carolina-Reaper-Pepper.jpg"></div>   
+        <div ><img class="productimage" src="Image\TrinidadMoruga.jpg"></div>   
         <div class="productinfo">
-            <h5>Carolina Reaper</h5>
-            <h6>$99.99</h6>
+            <h5>Trinidad Moruga</h5>
+            <h6>13.99€</h6>
+            <button class="addtocartBtn">Add to Cart</button>
+
         </div>
     </div>
     <div class="product-card">
-        <div ><img class="productimage" src="https://www.chilipeppermadness.com/wp-content/uploads/2019/04/Carolina-Reaper-Pepper.jpg"></div>   
+        <div ><img class="productimage" src="Image\ButchTaylor.jpg"></div>   
         <div class="productinfo">
-            <h5>Carolina Reaper</h5>
-            <h6>$99.99</h6>
+            <h5>Butch Taylor</h5>
+            <h6>12.99€</h6>
+            <button class="addtocartBtn">Add to Cart</button>
+
         </div>
     </div>
     <div class="product-card">
-        <div ><img class="productimage" src="https://www.chilipeppermadness.com/wp-content/uploads/2019/04/Carolina-Reaper-Pepper.jpg"></div>   
+        <div ><img class="productimage" src="Image\NagaViper.jpg"></div>   
         <div class="productinfo">
-            <h5>Carolina Reaper</h5>
-            <h6>$99.99</h6>
-        </div>
-    </div>
-    <div class="product-card">
-        <div ><img class="productimage" src="https://www.chilipeppermadness.com/wp-content/uploads/2019/04/Carolina-Reaper-Pepper.jpg"></div>   
-        <span class="productinfo">
-            <h5>Carolina Reaper</h5>
-            <h6>$99.99</h6>
-        </div>
-    </div>
-    <div class="product-card">
-        <div ><img class="productimage" src="https://www.chilipeppermadness.com/wp-content/uploads/2019/04/Carolina-Reaper-Pepper.jpg"></div>   
-        <div class="productinfo">
-            <h5>Carolina Reaper</h5>
-            <h6>$99.99</h6>
-        </div>
-    </div>
-    <div class="product-card">
-        <div ><img class="productimage" src="https://www.chilipeppermadness.com/wp-content/uploads/2019/04/Carolina-Reaper-Pepper.jpg"></div>   
-        <div class="productinfo">
-            <h5>Carolina Reaper</h5>
-            <h6>$99.99</h6>
-        </div>
-    </div>
-    <div class="product-card">
-        <div ><img class="productimage" src="https://www.chilipeppermadness.com/wp-content/uploads/2019/04/Carolina-Reaper-Pepper.jpg"></div>   
-        <div class="productinfo">
-            <h5>Carolina Reaper</h5>
-            <h6>$99.99</h6>
+            <h5>Naga Viper</h5>
+            <h6>11.99€</h6>
+            <button class="addtocartBtn">Add to Cart</button>
+
         </div>
     </div>
     </section>
