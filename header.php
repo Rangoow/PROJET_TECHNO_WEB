43204ff1a70da480d9b45a9680f28610484d3822
Rangoow <noe.esp@hotmail.fr>
2019-10-11 15:20:24 +0200

    adjustments 2

--- header.php
+++ header.php
@@ -7,9 +7,9 @@
  
     <div class="log-container">
       <form action="#">
-        <input type="text" placeholder="Username" name="username">
-        <input type="text" placeholder="Password" name="psw">
-        <button class="titleCategory"  href="">LOGIN</button>
+        <input type="text" placeholder="Username" name="Username">
+        <input type="Password" placeholder="Password" name="Password">
+        <button class="titleCategory"  href="main.php">LOGIN</button>
       </form>
     </div>
     <div class="log-container">
