2f283702d152d14787289bfaf937520f9a3c1cca
Rangoow <noe.esp@hotmail.fr>
2019-10-11 14:40:40 +0200

    css adjust & products aspects modifs

--- productPage.php
+++ productPage.php
@@ -15,16 +15,18 @@
     <div class="product-card">
         <div ><img class="productimage" src="Image\CarolinaReaper.jpg"></div>   
         <div class="productinfo">
-            <h5>Carolina Reaper</h5>
-            <h6>14.99€</h6>
+            <h5 class="productName">Carolina Reaper</h5>
+            <h6 class="productPrice">14.99€</h6>
+            <div class="productDescription">Description product 1 bla bla bla bla bla bla bla bla bla bla.</div>
             <button class="addtocartBtn">Add to Cart</button>
         </div>
     </div>
     <div class="product-card">
         <div ><img class="productimage" src="Image\TrinidadMoruga.jpg"></div>   
         <div class="productinfo">
-            <h5>Trinidad Moruga</h5>
-            <h6>13.99€</h6>
+            <h5 class="productName">Trinidad Moruga</h5>
+            <h6 class="productPrice">13.99€</h6>
+            <div class="productDescription">Description product 2 bla bla bla bla bla bla bla bla bla bla.</div>
             <button class="addtocartBtn">Add to Cart</button>
 
         </div>
@@ -32,8 +34,9 @@
     <div class="product-card">
         <div ><img class="productimage" src="Image\ButchTaylor.jpg"></div>   
         <div class="productinfo">
-            <h5>Butch Taylor</h5>
-            <h6>12.99€</h6>
+            <h5 class="productName">Butch Taylor</h5>
+            <h6 class="productPrice">12.99€</h6>
+            <div class="productDescription">Description product 3 bla bla bla bla bla bla bla bla bla bla.</div>
             <button class="addtocartBtn">Add to Cart</button>
 
         </div>
@@ -41,8 +44,9 @@
     <div class="product-card">
         <div ><img class="productimage" src="Image\NagaViper.jpg"></div>   
         <div class="productinfo">
-            <h5>Naga Viper</h5>
-            <h6>11.99€</h6>
+            <h5 class="productName">Naga Viper</h5>
+            <h6 class="productPrice">11.99€</h6>
+            <div class="productDescription">Description product 4 bla bla bla bla bla bla bla bla bla bla.</div>
             <button class="addtocartBtn">Add to Cart</button>
 
         </div>
